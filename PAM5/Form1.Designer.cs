452b92855609c108c78210de3d3155a5a05f0a00
tired-zoid <rahimzoid@gmail.com>
2022-11-18 15:10:47 +0300

    cc

---
+++ PAM5/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace PAM5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.HorizontalCenter;
+            this.chart1.BorderSkin.BorderWidth = 10;
+            chartArea1.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea1.AxisX.InterlacedColor = System.Drawing.Color.White;
+            chartArea1.AxisX.IsInterlaced = true;
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisX.LabelStyle.Interval = 1D;
+            chartArea1.AxisX.MajorGrid.Interval = 1D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea1.AxisY.Interval = 1D;
+            chartArea1.AxisY.MajorGrid.Interval = 1D;
+            chartArea1.AxisY.Maximum = 2D;
+            chartArea1.AxisY.Minimum = -2D;
+            chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(62, 290);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series1.BackSecondaryColor = System.Drawing.Color.Red;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series1.EmptyPointStyle.Color = System.Drawing.Color.Maroon;
+            series1.IsVisibleInLegend = false;
+            series1.LabelBorderWidth = 5;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Сигнал";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(960, 287);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Top;
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "Title1";
+            title1.Text = "PAM 5 ";
+            this.chart1.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(663, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Кодировать ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(472, 11);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 63);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(346, 55);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Показать входной сигнал";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите комбинацию нулей и единиц для кодирования:";
+            // 
+            // chart2
+            // 
+            this.chart2.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.chart2.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.HorizontalCenter;
+            this.chart2.BorderSkin.BorderWidth = 10;
+            chartArea2.AxisX.Crossing = 0D;
+            chartArea2.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea2.AxisX.InterlacedColor = System.Drawing.Color.White;
+            chartArea2.AxisX.Interval = 1D;
+            chartArea2.AxisX.IsInterlaced = true;
+            chartArea2.AxisX.IsMarginVisible = false;
+            chartArea2.AxisX.LabelStyle.Interval = 1D;
+            chartArea2.AxisX.LabelStyle.IntervalOffset = 0D;
+            chartArea2.AxisX.MajorGrid.Interval = 1D;
+            chartArea2.AxisX.MajorTickMark.Interval = 1D;
+            chartArea2.AxisX.MajorTickMark.IntervalOffset = 0D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisX2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea2.AxisY.Interval = 1D;
+            chartArea2.AxisY.MajorGrid.Interval = 1D;
+            chartArea2.AxisY.Maximum = 1D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(62, 91);
+            this.chart2.Margin = new System.Windows.Forms.Padding(4);
+            this.chart2.Name = "chart2";
+            series2.BackSecondaryColor = System.Drawing.Color.Red;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series2.EmptyPointStyle.Color = System.Drawing.Color.Maroon;
+            series2.IsVisibleInLegend = false;
+            series2.LabelBorderWidth = 5;
+            series2.Legend = "Legend1";
+            series2.LegendText = "Сигнал";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(960, 233);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            title2.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Top;
+            title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title2.Name = "Title1";
+            title2.Text = "Входной сигнал ";
+            this.chart2.Titles.Add(title2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(522, 576);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PAM 5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.ClientSize = new System.Drawing.Size(1070, 617);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PAM 5";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PAM5/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PAM5
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+             textBox2.Visible = false;
+            //  chart1.Visible = false;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].AxisX.IsStartedFromZero = true;
+            chart1.ChartAreas[0].AxisX.IntervalOffset = 0;
+            chart1.ChartAreas[0].AxisY.IntervalOffset = 0;
+
+            string code = textBox1.Text;
+            string[] SS = new string[50];
+
+            int k = 0;
+            int i;
+            for (i = 0; i < code.Length - 1; i = i + 2)
+            {
+                string S1 = code.Substring(i, 2);
+                SS[k++] = S1;
+            }
+
+            //for (i = 0; i < SS.Length; i++)
+            //{
+            //    textBox2.Text += SS[i] + " ";
+            //}
+
+
+            SS = SS.Where(q => q != null).ToArray();
+
+
+            for (i = 0; i < SS.GetLength(0); i++) // кодирование входного сигнала 
+            {
+                if (SS[i] == "00")
+                    SS[i] = "-2";
+                if (SS[i] == "01")
+                    SS[i] = "-1";
+                if (SS[i] == "10")
+                    SS[i] = "1";
+                if (SS[i] == "11")
+                    SS[i] = "2";
+            }
+
+            int[] intArray = new int[SS.Length];
+
+            for (i = 0; i < SS.Length; i++) // конвертирование закодированного сигнала в массив чисел
+            {
+                if (SS[i] != null)
+                    intArray[i] = int.Parse(SS[i]);
+            }
+
+            int[] intA = new int[SS.Length * 2];
+
+            int a = 0;
+            int j = 0;
+            while (a < intArray.Length) // дублирование закодированного массива 
+            {
+                while (j < intA.Length)
+                {
+
+                    intA[j] = intArray[a];
+                    j++;
+                    intA[j] = intArray[a];
+                    a++;
+                    j++;
+                }
+
+            }
+
+            Array.Resize(ref intA, intA.Length + 1);
+            int z = 0;
+            z = intA[intA.Length - 2];
+            intA[intA.Length - 1] = z;
+
+            int[] x = new int[intA.Length];
+            for (i = 1; i < x.Length; i++)
+            {
+                x[i] = i;
+            }
+
+            for (i = 0; i < intA.GetLength(0); i++)
+            {
+                if (intA[i] != 0)
+                    chart1.Series["Series1"].Points.DataBindXY(x, intA);
+            }
+           // chart1.ChartAreas[0].AxisX.Maximum = x.Length;
+            chart1.Series[0].BorderWidth = 3;
+            //   chart1.Visible = true;
+            chart1.Series[0].Color = Color.Black;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            string SS = textBox1.Text;
+            string[] W = SS.Split(' ');
+
+            int[] intArray = new int[SS.Length];
+
+            for (int i = 0; i < SS.Length; i++) // конвертирование массива строк в массив чисел 
+            {
+                intArray[i] = Convert.ToInt32(SS[i].ToString());
+            }
+
+            
+                Array.Resize(ref intArray, intArray.Length + 1);
+                int dob = 0;
+                dob = intArray[intArray.Length - 2];
+                intArray[intArray.Length - 1] = dob;
+            
+
+
+                for (int i = 0; i < intArray.Length; i++)
+                {
+                    textBox2.Text += intArray[i] + " ";
+                }
+                int[] x = new int[intArray.Length];
+                for (int i = 0; i < x.Length; i++)
+                {
+                    x[i] = i;
+                }
+                chart1.ChartAreas[0].AxisX.Maximum = x.Length - 1;
+                chart2.Series["Series1"].Points.DataBindXY(x, intArray);
+                chart2.Series[0].BorderWidth = 3;
+                chart2.Series[0].Color = Color.Black;
+      
+
+
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                if (e.KeyChar == '0' || e.KeyChar == '1' || e.KeyChar == (char)Keys.Back)
+
+                    e.Handled = false;
+                else
+                    e.Handled = true;
+            }
+        }
+    }
+
+
+
+
+
